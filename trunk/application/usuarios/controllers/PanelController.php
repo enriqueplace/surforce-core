5081954a3ec39027399538f9f6d508c1f9918b57
enriqueplace <enriqueplace@3ac336b0-ddd6-11dd-a893-8d7ce9fbef62>
2009-02-12 18:48:38 +0000

    Primera versión de "cambio de clave"

---
+++ trunk/application/usuarios/controllers/PanelController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * IndexController - The default controller class
+ * 
+ * @author
+ * @version 
+ */
+
+class Usuarios_PanelController extends Zsurforce_Generic_ControllerAdmin
+{
+    public function init()
+    {
+        parent::init();
+    }
+    public function indexAction() 
+    {
+        
+    }
+    public function claveAction()
+    {
+
+    }
+}
---
+++ trunk/application/usuarios/views/scripts/panel/main.js
@@ -0,0 +1,3 @@
+function cambiar_clave_validar(){
+    alert('prueba');
+}
