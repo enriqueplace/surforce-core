169155d91eec7246421a7796975767f66a5807fb
enriqueplace <enriqueplace@3ac336b0-ddd6-11dd-a893-8d7ce9fbef62>
2009-02-12 20:31:42 +0000

    Agrega primer versión funciona de cambio de clave

--- application/usuarios/controllers/PanelController.php
+++ application/usuarios/controllers/PanelController.php
@@ -6,6 +6,7 @@
  * @author
  * @version 
  */
+require_once '../application/default/models/Usuarios.php';
 
 class Usuarios_PanelController extends Zsurforce_Generic_ControllerAdmin
 {
@@ -19,6 +20,23 @@ class Usuarios_PanelController extends Zsurforce_Generic_ControllerAdmin
     }
     public function claveAction()
     {
+        if ($this->_request->isPost()) {
 
+            Zend_Loader::loadClass('Zend_Filter_StripTags');
+
+            $f = new Zend_Filter_StripTags();
+
+            $pass1  = $f->filter($this->_request->getPost('pass1'));
+            $pass2 	= $f->filter($this->_request->getPost('pass2'));
+
+            if ($pass1 != $pass2){
+                $this->view->mensajeError = '¡Las claves ingresadas no coinciden!';
+            } else {
+
+                $model = new Models_Usuarios();
+                $model->setPassword($pass1, $this->_user->usuario_id);
+                $this->view->mensajeInformativo = '¡Clave Actualizada!';
+            }
+        }
     }
 }
