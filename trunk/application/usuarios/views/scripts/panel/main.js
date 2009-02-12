1243bf3194e508f0ac9e59c412409cfb757877be
enriqueplace <enriqueplace@3ac336b0-ddd6-11dd-a893-8d7ce9fbef62>
2009-02-12 20:31:42 +0000

    Agrega primer versión funciona de cambio de clave

--- trunk/application/usuarios/views/scripts/panel/main.js
+++ trunk/application/usuarios/views/scripts/panel/main.js
@@ -1,3 +1,15 @@
 function cambiar_clave_validar(){
-    alert('prueba');
+    
+    if( $('#pass1').val() != $('#pass2').val()){
+        alert('¡Las claves no pueden ser distintas!');
+
+        $('#pass1').val('');
+        $('#pass2').val('');
+
+        $('#pass1').focus();
+
+        return false;
+    }
+    $('#cambiar-clave-form').submit();
+
 }
